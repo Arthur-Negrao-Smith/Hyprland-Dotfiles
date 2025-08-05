1d1604ee9474e609eefa373b80dcff8a08aecb4a
Arthur Negrão <arthurnegrao10s@gmail.com>
2025-08-04 22:37:29 -0300

    feat: Add notification whenever change the wallpaper.

--- scripts/.config/scripts/wallpaper.sh
+++ scripts/.config/scripts/wallpaper.sh
@@ -6,5 +6,8 @@ CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 # Get a random wallpaper that is not the current one
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
 
+# send a notification
+notify-send -a "System" "Your wallpaper was changed to '$WALLPAPER'"
+
 # Apply the selected wallpaper
 hyprctl hyprpaper reload ,"$WALLPAPER"
