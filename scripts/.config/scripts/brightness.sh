9c172a03017ce1360ffb7792914a942649caf2f3
Arthur Negrão <arthurnegrao10s@gmail.com>
2025-08-13 21:47:52 -0300

    feat: Add brightness control with Super + ctrl + "+" or "-".

---
+++ scripts/.config/scripts/brightness.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+STEP=10
+ACTION=$1
+
+MONITOR_1=4
+MONITOR_2=5
+
+case $ACTION in
+    up)
+        ddcutil -b $MONITOR_1 setvcp 10 + $STEP
+        ddcutil -b $MONITOR_2 setvcp 10 + $STEP
+    ;;
+    down)
+        ddcutil -b $MONITOR_1 setvcp 10 - $STEP
+        ddcutil -b $MONITOR_2 setvcp 10 - $STEP
+    ;;
+esac
+
+brightness=$(ddcutil getvcp 10 | awk -F '=' '{print $2}' | awk '{print $1}' | tr -d ' ' | tr -d ',')
+notify-send -a "System" "The monitor brightness is $brightness%"
